4451d9c267d23a8679df02e6c26633ca6df0ccd2
KLDevOpsteam <bijua6814@gmail.com>
2025-08-09 18:09:07 +0530

    final commit

--- highschool.py
+++ highschool.py
@@ -1,1 +1,20 @@
-#Testing for Github
+print("Hi World")
+x=int(input("Enter a value"))
+y=7
+z=6
+if (x>y and x>z):
+    print("largest no",x)
+elif (y>z):
+    print("largest no",y)
+else:
+    print("largest no",z)
+if (x%2==0):
+    print(f"{x} is even")
+else:
+    print(f"{x} is odd")
+fact =1
+for i in range (1,x+1):
+    fact = fact*i 
+   # print (fact) 
+    
+print (fact)
