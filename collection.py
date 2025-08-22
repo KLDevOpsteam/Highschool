f422ac6f718edb94d61fab1b8f8f8ca50aafc0c3
KLDevOpsteam <bijua6814@gmail.com>
2025-08-22 11:25:58 +0530

    220825

--- collection.py
+++ collection.py
@@ -84,3 +84,54 @@ for quantity,price in zip(quantities,prices):
     TotalAmount += Total
 
 print ("Total Amount to pay =",TotalAmount)
+
+
+#2D collections --Like a table
+
+#Exercise : Number pad
+
+num_pad1 =  (1,2,3)
+num_pad2=   (4,5,6)
+num_pad3=   (7,8,9)
+num_pad4=   ("#",0,"*")
+
+#Aligned each tuple orderly makes each tuple as an element in num_pad
+#num_pad=( (1,2,3),(4,5,6),(7,8,9),("#",0,"*"))     this one also give same output as below .making 2d collection in one sentance
+#can make 2D collection with [],(),{}
+num_pad=(num_pad1,num_pad2,num_pad3,num_pad4)
+#print(num_pad[0][1])    it will give output as 1st row 2nd element.
+#for x in num_pad:
+#     for y in x:
+#          print(y) #this will give output of each elements in tuple.
+#           print(y,end=" ")
+#      print() # this will give each tuple in line
+    
+for numbers in num_pad:
+    for num in numbers:
+        print(num,end=" ")
+    print()
+
+
+#Exercise  Python quize Game
+
+Questions=("How many continents are present?","which flower is biggest in the world","Which day is celebrated as Environmental Day")
+Options=(("A.7","B.4","C.9"),("A.Rose","B.orchid","C.Rufflasia"),("A.June10","B.Jul5","C.March12"))
+answers=("A","C","B")
+guesses=[]
+score=0
+question_num=0
+
+for question in Questions:
+    print(question)
+    for option in Options[question_num]:
+        print(option)
+    guess= input("Enter the answer(A,B,C): ").upper()
+    guesses.append(guess)
+    if guess == answers[question_num]:
+            score += 1
+            print("Correct")
+    else:
+         print("Incorrect")
+         print(f"Correct Answer is {answers[question_num]}")
+    question_num += 1   
+print(f"Your score is {score} ")
